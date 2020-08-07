b8e128847b451166c5166cb526cfe5ed0921ddbc
parth23571 <pawal.sateesh@gmail.com>
2020-08-07 09:45:11 +0530

    Another scripts

---
+++ application/src/test/java/com/test/application/CaptureData.java
@@ -0,0 +1,23 @@
+package com.test.application;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CaptureData {
+  @Test
+  public void f1() {
+	try
+	{
+	String getText=LaunchApp.driver.findElement(By.xpath("//tbody//tr[2]//td[2]")).getText();
+System.out.println(getText);	
+	}
+	
+	catch(Exception e)
+	{
+		System.out.println(e.getMessage());
+	}
+	
+	}
+}
